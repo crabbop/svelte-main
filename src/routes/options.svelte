ec539539aec83d5b84fdcfaeef18a6845426923f
crabbop <59592196+crabbop@users.noreply.github.com>
2024-12-25 23:05:51 +1100

    this is a test

---
+++ src/routes/options.svelte
@@ -0,0 +1,12 @@
+<div class="options">
+    <h2>Options</h2>
+    <p>Man i hop this silly thing works</p>
+</div>
+
+<style>
+    .options {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+        }
+    </style>
